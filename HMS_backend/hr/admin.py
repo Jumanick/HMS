d75d477e2b81ddcd3d7f3b4303c5f4833cb209fd
nick juma <nickoj640@gmail.com>
2026-08-14 14:40:11 +0300

    configured django admin interface for EmploProfile and SalaryRecord with inline editing and filtering

--- HMS_backend/hr/admin.py
+++ HMS_backend/hr/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import EmployeeProfile, SalaryRecord
 
-# Register your models here.
+
+class SalaryRecordInline(admin.TabularInline):
+    model = SalaryRecord
+    extra = 0
+
+
+@admin.register(EmployeeProfile)
+class EmployeeProfileAdmin(admin.ModelAdmin):
+    list_display = ('employee_number', 'first_name', 'last_name', 'department', 'position', 'employment_status')
+    list_filter = ('department', 'employment_status')
+    search_fields = ('employee_number', 'first_name', 'last_name')
+    inlines = [SalaryRecordInline]
+
+
+@admin.register(SalaryRecord)
+class SalaryRecordAdmin(admin.ModelAdmin):
+    list_display = ('employee', 'net_pay', 'pay_period_start', 'pay_period_end', 'status')
+    list_filter = ('status',)
